7ee61cf6c35d82b98b1418c73c5426b112883bba
mengziheng <hitmzh@163.com>
2023-06-30 21:36:50 +0800

    a

--- preprocess/sortbydegree.cpp
+++ preprocess/sortbydegree.cpp
@@ -29,35 +29,6 @@ bool cmp_degree(struct node a, struct node b)
 {
     return (a.degree > b.degree) || (a.degree == b.degree && a.id < b.id);
 }
-void removeDuplicatedEdgeAndSelfLoop(shared_ptr<vector<int>[]> b)
-{
-    for (int i = 0; i < vertexCount; i++)
-    {
-        sort(b[i].begin(), b[i].end());
-        vector<int> a;
-        int previous = -1;
-        for (auto item : b[i])
-        {
-            if (item != previous)
-            {
-                previous = item;
-                if (item != i)
-                    a.push_back(item);
-            }
-        }
-        b[i].swap(a);
-    }
-}
-void printb(shared_ptr<vector<int>[]> b)
-{
-    for (int i = 0; i < vertexCount; i++)
-    {
-        cout << i << ":";
-        for (auto dst : b[i])
-            cout << dst << ' ';
-        cout << endl;
-    }
-}
 
 void loadEdgeList(string path)
 {
@@ -181,105 +152,6 @@ void loadEdgeList(string path)
     propertiesFile.close();
 }
 
-void loadGeneratedGraph(string path)
-{
-    fstream propertiesInFile(path + "/properties1.txt", ios::in);
-    long long vertexpower, degree;
-    // propertiesInFile >> vertexpower >> degree;
-    // vertexCount = powl(2, vertexpower);
-    // edgeCount = degree * vertexCount;
-    propertiesInFile >> vertexCount >> edgeCount;
-    propertiesInFile.close();
-    unique_ptr<uint32_t[]> a(new uint32_t[edgeCount * 2]);
-    fstream file(path + "/edgelist", ios::in | ios::binary);
-    file.read((char *)&a[0], sizeof(uint32_t) * (edgeCount)*2);
-    file.close();
-
-    shared_ptr<vector<int>[]> b(new vector<int>[vertexCount]);
-    for (int i = 0; i < edgeCount; i++)
-    {
-        int u = a[i * 2], v = a[i * 2 + 1];
-        b[u].push_back(v);
-        b[v].push_back(u);
-    }
-
-    removeDuplicatedEdgeAndSelfLoop(b);
-    struct node *idDegree = new struct node[vertexCount];
-    for (int i = 0; i < vertexCount; i++)
-    {
-        idDegree[i].id = i;
-        idDegree[i].degree = b[i].size();
-    }
-    sort(idDegree, idDegree + vertexCount, cmp_degree);
-    unique_ptr<int[]> newid(new int[vertexCount]);
-    for (int i = 0; i < vertexCount; i++)
-    {
-        newid[idDegree[i].id] = i;
-    }
-    for (int i = 0; i < vertexCount; i++)
-    {
-        for (auto &dst : b[i])
-            dst = newid[dst];
-        sort(b[i].begin(), b[i].end());
-    }
-    ofstream beginFile(path + "/begin.bin", ios::out | ios::binary);
-    ofstream adjFile(path + "/adj.bin", ios::out | ios::binary);
-    long long sum = 0;
-    for (int i = 0; i < vertexCount; i++)
-    {
-        int vertex = idDegree[i].id;
-        beginFile.write((char *)&sum, sizeof(long long));
-        sum += idDegree[i].degree;
-        adjFile.write((char *)&b[vertex][0], sizeof(int) * idDegree[i].degree);
-    }
-    beginFile.write((char *)&sum, sizeof(long long));
-    ofstream propertiesFile(path + "/properties.txt", ios::out);
-    propertiesFile << 0 << ' ' << vertexCount << ' ' << sum << endl;
-    beginFile.close();
-    adjFile.close();
-    propertiesFile.close();
-}
-
-void sortbydegree()
-{
-    int n = vertexCount;
-    nnn = new struct node[n];
-    for (int i = 0; i < n; i++)
-    {
-        nnn[i].id = i;
-        nnn[i].degree = deg[i];
-    }
-    sort(nnn, nnn + n, cmp_degree);
-}
-void storeCSR(string path)
-{
-    int *a = new int[vertexCount];
-
-    for (int i = 0; i < vertexCount; i++)
-    {
-        a[nnn[i].id] = i;
-    }
-
-    // give each edge new id
-    for (int i = 0; i < edgeCount; i++)
-        edgeList[i] = a[edgeList[i]];
-    ofstream beginFile(path + "/begin.bin", ios::out | ios::binary);
-    ofstream adjFile(path + "/adj.bin", ios::out | ios::binary);
-    // beginFile.write((char*)beginPos,sizeof(long long)*(uCount+vCount+1));
-    // adjFile.write((char*)edgeList,sizeof(int)*(edgeCount));
-    long long sum = 0;
-    for (int i = 0; i < vertexCount; i++)
-    {
-        int vertex = nnn[i].id;
-        beginFile.write((char *)&sum, sizeof(long long));
-        int degree = beginPos[vertex + 1] - beginPos[vertex];
-        sum += degree;
-        adjFile.write((char *)&edgeList[beginPos[vertex]], sizeof(int) * degree);
-    }
-    beginFile.write((char *)&sum, sizeof(long long));
-    ofstream propertiesFile(path + "/properties.txt", ios::out);
-    propertiesFile << uCount << ' ' << vCount << ' ' << edgeCount << endl;
-}
 int main(int argc, char *argv[])
 {
     string path;
@@ -294,13 +166,5 @@ int main(int argc, char *argv[])
         isGenerated = atoi(argv[2]) > 0;
     }
     cout << path << endl;
-    if (isGenerated)
-    {
-        loadGeneratedGraph(path);
-    }
-    else
-    {
-        cout << "here" << endl;
-        loadEdgeList(path);
-    }
+    loadEdgeList(path);
 }
